c2ed2066f4cee9071c859266a36d5160400154e4
MxMoss3 <mossberg.max@gmail.com>
2017-09-09 09:53:00 -0400

    mario's stairs now work

--- mario_stairs.py
+++ mario_stairs.py
@@ -1,4 +1,18 @@
-height = input("How high?(0-23")
+height = int(input("How high?(0-23)"))
 
-while valid = False:
+#checks if the input is valid before continuing
+input_good = False
+while input_good == False:
+    if height < 0 or height > 23:
+        height = int(input("How high?(0-23)"))
+        pass
+    else:
+        input_good = True
 
+#prints the stairs
+for i in range(height):
+    print((' ' * (height - (i + 1))) + 
+            ('#' * (i + 1)) +
+            ('  ') + 
+            ('#' * (i + 1)) +
+            (' ' * (height - (i + 1))))  
